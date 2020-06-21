23e7704306b4d96378f761c53da4d792cd39d407
lordfme <fortmax120@gmail.com>
2020-06-21 10:50:59 +0100

    Added IP option, fix EOF bug, sped up ready port selection, minor fixes

--- ptpclient.py
+++ ptpclient.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2
+
 import socket
 import argparse
 import select
@@ -8,14 +10,14 @@ from operator import add
 def hit_port(client_port, server_port):
     client_socket = socket.socket()
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    client_socket.bind(("0.0.0.0", client_port))
-    print ("Hitting port " + str(server_port))
+    if client_port:
+        client_socket.bind((client_ip, client_port))
+    print >>stderr, "Hitting port " + str(server_port)
     client_socket.connect((server_ip, server_port))
     client_socket.close()
 
 
 def handle_ports(client_port, server_port):
-    # print >> stderr, "Handling port", port
     if server_port > (server_offset + max_index):
         # Handle special case server_port
         server_port -= (server_offset + max_index)
@@ -35,39 +37,36 @@ def handle_ports(client_port, server_port):
     return (index, bit_seq)
 
 
-# READ_ONLY = (select.POLLIN | select.POLLPRI | select.POLLPRI | select.POLLHUP | select.POLLERR)
-# READ_WRITE = READ_ONLY | select.POLLOUT
-# poller = select.poll()
-# poller.register(server, READ_ONLY)
-
 ap = argparse.ArgumentParser()
 
 ap.add_argument("-O","--server_offset",default=34000,type=int,help="Number of ports to step over before mapping offset+1, ..., to indexes. Default 34000 (in case running both server and client on same machine limit clashes)")
 ap.add_argument("-o","--client_offset",default=1024,type=int,help="Number of ports to step over before mapping offset+1, ..., to bit sequences. Default 1024 (running non-root)",)
 ap.add_argument("-m","--max_index",default=248,type=int,help="Number of bit-sequences to send before waiting for acknowledgment from client",)
 ap.add_argument("-b","--bits",default=8,type=int,help="Bit space assigned to each port. Default 8 bits",)
+ap.add_argument("-i", "--ip", default="0.0.0.0", type=str, help="IP address of this machine. Default 0.0.0.0")
 args = vars(ap.parse_args())
 
+client_ip = args["ip"]
 if args["bits"] < 4:
-    print "Minimum bits is 4, using ", 4
+    print >>stderr, "Minimum bits is 4, using ", 4
 elif args["bits"] > 16:
-    print "Maximum bits exceeded, using ", 16
+    print >>stderr, "Maximum bits exceeded, using ", 16
 bits = max(min(args["bits"], 16), 4)
 
 if args["client_offset"] > 65534 - 2 ** bits + 2:
-    print "Client Offset value exceeded, using ", 65534 - 2 ** bits + 2
+    print >>stderr, "Client Offset value exceeded, using ", 65534 - 2 ** bits + 2
 client_offset = min(args["client_offset"], 65534 - 2 ** bits + 2)
 
 if args["max_index"] > 2 ** bits - 8:
-    print "Max index value exceeded, using ", 2 ** bits - 8
+    print >>stderr, "Max index value exceeded, using ", 2 ** bits - 8
 elif args["max_index"] % 8:
-    print "Max index must be divisible by 8, using ", args["max_index"] / 8 * 8
+    print >>stderr, "Max index must be divisible by 8, using ", args["max_index"] / 8 * 8
 elif args["max_index"] == 0:
-    print "Invalid Max index, using minimum value 8"
+    print >>stderr, "Invalid Max index, using minimum value 8"
 max_index = max(min(args["max_index"] / 8 * 8, 2 ** bits - 8), 8)
 
 if args["server_offset"] > 65535 - 19 - max_index:
-    print "Server Offset value exceeded, using ", 65535 - 19 - max_index
+    print >>stderr, "Server Offset value exceeded, using ", 65535 - 19 - max_index
 server_offset = min(args["server_offset"], 65535 - 19 - max_index)
 
 
@@ -76,27 +75,32 @@ port_array = []
 bit_buffer = [''] * max_index
 eof = False
 
-for port in range(client_offset + 1, client_offset + 2 ** bits - 2):
+for port in range(client_offset + 1, client_offset + 2 ** bits - 1):
     sock = socket.socket()
-    sock.bind(("0.0.0.0", port))
+    sock.bind((client_ip, port))
     sock.listen(1)
     port_array.append(sock)
 
 while not eof:
-    for count in range(max_index):
+    count = 0
+    while count < max_index:
         readable, _, _ = select.select(port_array, [], [])
-        ready_server = readable[0]
-        client_port = ready_server.getsockname()[1]
-        recv_socket, (server_ip, server_port) = ready_server.accept()
-        recv_socket.close()
-        index, bit_seq = handle_ports(client_port, server_port)
-        if bit_seq.startswith('-'):
-            # Handle EOF
-            bit_buffer[index-1] = bit_buffer[index-1][:int(bit_seq)]
-            eof = True
+        for ready_server in readable:
+            count += 1
+            client_port = ready_server.getsockname()[1]
+            recv_socket, (server_ip, server_port) = ready_server.accept()
+            recv_socket.close()
+            index, bit_seq = handle_ports(client_port, server_port)
+            if bit_seq.startswith('-'):
+                # Handle EOF
+                # bit_buffer[index-1] = bit_buffer[index-1][:int(bit_seq)]
+                eof = True
+                max_index = index + 1
+            else:
+                bit_buffer[index] = bit_seq
+        if eof:
             break
-        else:
-            bit_buffer[index] = bit_seq
     print reduce(add, bit_buffer)
     if not eof:
-        hit_port(65534, 65535)
+        bit_buffer = [''] * max_index
+        hit_port(0, 65535)
