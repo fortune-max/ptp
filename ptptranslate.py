c7143160708778553050f07e7b5a471dacc403c5
lordfme <fortmax120@gmail.com>
2020-06-25 18:04:26 +0100

    ported translator to py3

--- ptptranslate.py
+++ ptptranslate.py
@@ -1,11 +1,12 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python3
 
 import argparse
 from sys import stdout, stdin
 from operator import add
+from functools import reduce
 
 ap = argparse.ArgumentParser(
-    formatter_class=argparse.RawDescriptionHelpFormatter, description=" ", epilog=" "
+    formatter_class=argparse.RawDescriptionHelpFormatter, description="", epilog=""
 )
 ap.add_argument("-i", "--input", default="-", type=str, help="")
 args = vars(ap.parse_args())
@@ -14,11 +15,11 @@ input_stream = args["input"]
 if input_stream == "-":
     raw_file = stdin
 else:
-    raw_file = open(input_stream, "rb")
+    raw_file = open(input_stream, "r")
 
 bit_array = raw_file.read().split()
 if bit_array:
     bits = reduce(add, bit_array)
-    for idx in range(len(bits) / 8):
+    for idx in range(int(len(bits) / 8)):
         byte = bits[idx * 8 : (idx + 1) * 8]
         stdout.write(chr(int(byte, 2)))
