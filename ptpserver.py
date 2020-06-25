4f587f953ad9fcfe943aef830c0ee53f789c86d9
lordfme <fortmax120@gmail.com>
2020-06-25 15:53:45 +0100

    removed threading, added poll_port option, closed poll socket

--- ptpserver.py
+++ ptpserver.py
@@ -2,33 +2,20 @@
 
 import socket
 import argparse
-import logging
-import concurrent.futures
 from sys import stdin, stderr
 from math import ceil
 from operator import add
 from functools import reduce
-from time import sleep
-
-
-def safe_connect(server_socket, server_port, client_port):
-    while True:
-        try:
-            server_socket.connect((client, client_port))
-            server_socket.close()
-            return True
-        except:
-            sleep(0.1)
 
 
 def hit_port(server_port, client_port):
     server_socket = socket.socket()
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((server_ip, server_port))
-    # server_socket.connect((client, client_port))
-    safe_connect(server_socket, server_port, client_port)
     # print ("Hitting port ", server_port, client_port, file=stderr)
-    # server_socket.close()
+    server_socket.connect((client, client_port))
+    server_socket.close()
+
 
 def resolve_ports(bit_seq, server_is_idx, idx):
     server_port = server_offset + 1 + [0, idx][server_is_idx]
@@ -45,20 +32,13 @@ def resolve_ports(bit_seq, server_is_idx, idx):
         if server_is_idx:
             return resolve_ports(bit_seq, False, idx)
         global eof_offset
-        eof_offset = bits - len(bit_seq) + 1  # eof_offset = 2, EOF-1
+        eof_offset = bits - len(bit_seq) + 1
+
         resolve_ports(bit_seq.ljust(bits, "0"), True, idx)
         client_port += 1
         server_port += max_index + eof_offset + 1
         print ("Sending EOF-%d" % (eof_offset - 1))
     hit_port(server_port, client_port)
-
-
-def iter_bits(params):
-    (bit_seq, start, stop, step) = params
-    for idx in range(start, stop, step):
-        to_send = bit_seq[idx * bits : (idx + 1) * bits]
-        resolve_ports(to_send, True, idx)
-        
 
 
 ap = argparse.ArgumentParser(
@@ -84,9 +64,9 @@ binary(1), binary(2), ..., binary(2^${bits}-2)           [binary sequences are z
 Some server ports represent something other than indexes:
 The EOF bits are sent at the end of the broadcast to know how many bits to right strip from received data. EOF-4 means strip off last 4 bits from sequence then terminate listening on client as all data has been received.
 The null bit is also sent from server-side as it cannot be squeezed into the client port when ${bits} value is 16.
-The last bit-sequence in bit-space is also sent from server side, as port 65535 is reserved for polling server (to avoid clash when ${bits} is 16)
+The last bit-sequence in bit-space is also sent from server side, as port ${poll_port} is reserved for polling server (to avoid clash when ${bits} is 16)
 
-Each time the server sends the next set of ${max_index} bit-sequences, it waits by listening on port 65535 for the client to state it has received, processed and properly ordered all bit-sequences received, using the accompanying indexes inferred from ${server_port}. The next set of ${max_index} bit-sequences are then sent and the process repeated till completion. This is to prevent ambiguity in where to position bits and ensure the transfer runs as quickly as (possibly varying) network speeds allow.""",
+Each time the server sends the next set of ${max_index} bit-sequences, it waits by listening on port ${poll_port} for the client to state it has received, processed and properly ordered all bit-sequences received, using the accompanying indexes inferred from ${server_port}. The next set of ${max_index} bit-sequences are then sent and the process repeated till completion. This is to prevent ambiguity in where to position bits and ensure the transfer runs as quickly as (possibly varying) network speeds allow.""",
 )
 
 ap.add_argument("-O","--server_offset",default=34000,type=int,help="Number of ports to step over before mapping offset+1, ..., to indexes. Default 34000 (in case running both server and client on same machine limit clashes)",)
@@ -96,13 +76,13 @@ ap.add_argument("-b","--bits",default=8,type=int,help="Bit space assigned to eac
 ap.add_argument("-f", "--file", default="-", type=str, help="Input file to serve. Default stdin")
 ap.add_argument("-i","--ip",default="0.0.0.0",type=str,help="IP address of this machine. Default 0.0.0.0",)
 ap.add_argument("-c","--client",default="127.0.0.1",type=str,help="Client IP to serve file to. Default localhost",)
-ap.add_argument("-t","--threads",default=8,type=int,help="Number of threads to spawn to handle port hitting",)
+ap.add_argument("-p","--poll_port",default=65535,type=int,help="Port to hit server on to receive next set of bits. Default 65535",)
 args = vars(ap.parse_args())
 
 client = args["client"]
 input_stream = args["file"]
 server_ip = args["ip"]
-threads = args["threads"]
+poll_port = args["poll_port"]
 
 if args["bits"] < 4:
     print ("Minimum bits is 4, using ", 4, file=stderr)
@@ -137,7 +117,7 @@ idx = 0
 # Set up server-poll socket
 wait_socket = socket.socket()
 wait_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-wait_socket.bind((server_ip, 65535))
+wait_socket.bind((server_ip, poll_port))
 wait_socket.listen(128)
 
 while True:
@@ -146,8 +126,10 @@ while True:
         break
     bit_seq = reduce(add, map(lambda x: bin(x)[2:].zfill(8), chunks))
     segments = min(max_index, int(ceil(len(bit_seq) / bits)))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=min(threads, segments)) as executor:
-        executor.map(iter_bits, ((bit_seq, i, segments, min(segments, threads)) for i in range(min(segments, threads))))
+    for idx in range(segments):
+        to_send = bit_seq[idx * bits : (idx + 1) * bits]
+        resolve_ports(to_send, True, idx)
+    
     # Wait for client ACK if not finished
     if segments == max_index:
         idx = -1  # Assert (idx + 2) % max_index is next index
@@ -162,5 +144,5 @@ if eof_offset == -1:
     client_port = client_offset + (idx + 2) % max_index
     server_port = server_offset + max_index + 3
     hit_port(server_port, client_port)
-
+wait_socket.close()
 print ("Done!")
