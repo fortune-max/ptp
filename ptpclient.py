3d95ca868cd71c323cc689352191ae40b74487a2
lordfme <fortmax120@gmail.com>
2020-07-30 19:07:03 +0100

    Modified to use UDP instead of TCP for transmissions

--- ptpclient.py
+++ ptpclient.py
@@ -9,11 +9,10 @@ from operator import add
 from functools import reduce
 
 
-def hit_port(client_port, server_port):
-    client_socket = socket.socket()
+def hit_port_tcp(client_port, server_port):
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    if client_port:
-        client_socket.bind((client_ip, client_port))
+    client_socket.bind((client_ip, client_port))
     # print ("Hitting port " + str(server_port), file=stderr)
     client_socket.connect((server_ip, server_port))
     client_socket.close()
@@ -25,22 +24,19 @@ def handle_ports(client_port, server_port):
         server_port -= server_offset + max_index
         if server_port == 1:
             bit_seq = "0" * bits
-            index = client_port - client_offset - 1
         elif server_port == 2:
             bit_seq = "1" * bits
-            index = client_port - client_offset - 1
         else:
-            bit_seq = "-%d" % (server_port - 3)  # -X in EOF-0, etc.
-            index = client_port - client_offset - 1
+            bit_seq = - (server_port - 3)  # -X in EOF-0, etc.
+        index = client_port - client_offset - 1
     else:
         # Handle server port as index and client as bit-sequence
         bit_seq = bin(client_port - client_offset)[2:].zfill(bits)
         index = server_port - server_offset - 1
-    return (index, bit_seq)
+    return (index + 1, bit_seq)
 
 
 ap = argparse.ArgumentParser()
-
 ap.add_argument("-O","--server_offset",default=34000,type=int,help="Number of ports to step over before mapping offset+1, ..., to indexes. Default 34000 (in case running both server and client on same machine limit clashes)",)
 ap.add_argument("-o","--client_offset",default=1024,type=int,help="Number of ports to step over before mapping offset+1, ..., to bit sequences. Default 1024 (running non-root)",)
 ap.add_argument("-m","--max_index",default=248,type=int,help="Number of bit-sequences to send before waiting for acknowledgment from client",)
@@ -48,13 +44,15 @@ ap.add_argument("-b","--bits",default=8,type=int,help="Bit space assigned to eac
 ap.add_argument("-i","--ip",default="0.0.0.0",type=str,help="IP address of this machine. Default 0.0.0.0",)
 ap.add_argument("-w", "--windows_mode", action="store_true", help="Run in Windows-compatible mode")
 ap.add_argument("-p","--poll_port",default=65535,type=int,help="Port to hit server on to receive next set of bits. Default 65535",)
-ap.add_argument("-v", "--verbose", action="store_true", help="display helpful stats, (slows performance)")
+ap.add_argument("-v", "--verbose", action="store_true", help="display helpful stats, (summary)")
+ap.add_argument("-V", "--Verbose", action="store_true", help="display helpful stats, (explicit)")
 args = vars(ap.parse_args())
 
 client_ip = args["ip"]
 windows_mode = args["windows_mode"]
 poll_port = args["poll_port"]
-verbose = args["verbose"]
+Verbose = args["Verbose"]
+verbose = args["verbose"] or Verbose
 
 if args["bits"] < 4:
     print ("Minimum bits is 4, using ", 4, file=stderr)
@@ -81,7 +79,9 @@ server_offset = min(args["server_offset"], 65535 - 19 - max_index)
 server_ip = None
 bit_buffer = [""] * max_index
 eof_state, eof_index, eof_offset = False, -1, -1
+last_buffer = ""
 start_step = 0
+max_speed = avg_speed = avg_count = 0
 
 if windows_mode:
     port_array = []
@@ -90,13 +90,19 @@ else:
     fd_to_socket = {}
     READ_ONLY = select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR
 
+# Set up server-poll TCP socket
+wait_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+wait_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+wait_socket.bind((client_ip, poll_port))
+wait_socket.listen(128)
+# recv_socket, (recv_ip, recv_port) = wait_socket.accept() #Use later
+
+# Setup UDP Data listeners
 for port in range(client_offset + 1, client_offset + 2 ** bits - 1):
-    sock = socket.socket()
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     # print ("listening on", port, file=stderr)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((client_ip, port))
-    # max value in /proc/sys/net/core/somaxconn, increase if higher than 128
-    sock.listen(128)
     if not windows_mode:
         poller.register(sock, READ_ONLY)
         fd_to_socket[sock.fileno()] = sock
@@ -105,35 +111,65 @@ for port in range(client_offset + 1, client_offset + 2 ** bits - 1):
 
 while not eof_state:
     count = 0
+    bit_buffer = [""] * max_index
+    missing_indexes = set(range(max_index))
+    bit_buffer = [last_buffer] + bit_buffer
     if verbose:
         step_duration = (time() - start_step)
         kbytes = max_index * bits / 8000
-        print ("%.2fKb/s"%(kbytes/step_duration), file=stderr)
+        speed = kbytes/step_duration
+        max_speed = max(speed, max_speed)
+        avg_speed = (avg_speed * avg_count + speed) / (avg_count + 1)
+        avg_count += 1
+        if Verbose:
+            print ("%.2fkB/s"%speed, file=stderr)
         start_step = time()
     while count < max_index:
-        if windows_mode:
-            readable, _, _ = select.select(port_array, [], [])
-        else:
-            readable = poller.poll()
-        for ready_server in readable:
-            if not windows_mode:
-                fd, flag = ready_server
-                ready_server = fd_to_socket[fd]
-            count += 1
-            client_port = ready_server.getsockname()[1]
-            recv_socket, (server_ip, server_port) = ready_server.accept()
-            recv_socket.close()
-            index, bit_seq = handle_ports(client_port, server_port)
-            if bit_seq.startswith("-"):
-                # Handle EOF
-                eof_state, eof_index = True, index
-                eof_offset = None if bit_seq == "-0" else int(bit_seq)
-                max_index = index + 1
+        readable = True
+        while count < max_index and readable:
+            # Recv data UDP
+            if windows_mode:
+                readable, _, _ = select.select(port_array, [], [], 30)
             else:
-                bit_buffer[index] = bit_seq
-        if eof_state:
-            bit_buffer[eof_index - 1] = bit_buffer[eof_index - 1][:eof_offset]
+                readable = poller.poll(30000)
+            for ready_server in readable:
+                if not windows_mode:
+                    fd, flag = ready_server
+                    ready_server = fd_to_socket[fd]
+                count += 1
+                client_port = ready_server.getsockname()[1]
+                recv_data, (server_ip, server_port) = ready_server.recvfrom(1)
+                index, bit_seq = handle_ports(client_port, server_port)
+                missing_indexes.remove(index-1)
+                if isinstance(bit_seq, int):
+                    # Handle EOF
+                    eof_state, eof_index = True, index
+                    eof_offset = [None, bit_seq][bool(bit_seq)]
+                    missing_indexes -= set(range(index, max_index))
+                    max_index = index
+                else:
+                    bit_buffer[index] = bit_seq
+        # wait for all UDP data sent signal
+        recv_socket, (server_ip, recv_port) = wait_socket.accept()
+        recv_socket.close()
+        # Send missing count (zero-indexed)
+        missing_count = len(missing_indexes)
+        hit_port_tcp(0, server_offset + missing_count + 1)
+        # wait for ACK of no missing
+        recv_socket, (server_ip, recv_port) = wait_socket.accept()
+        recv_socket.close()
+        # Send missing indexes (zero-indexed)
+        for missing_index in missing_indexes:
+            hit_port_tcp(0, server_offset + missing_index + 1)
+    # All data received
+    bit_buffer = bit_buffer[:count+1]
+    if eof_state:
+        bit_buffer[eof_index - 1] = bit_buffer[eof_index - 1][:eof_offset]
+        for sock in fd_to_socket.values() if not windows_mode else port_array:
+            sock.close()
+    last_buffer = bit_buffer.pop()
     print (reduce(add, bit_buffer))
-    if not eof_state:
-        bit_buffer = [""] * max_index
-        hit_port(0, poll_port)
+print(last_buffer)
+print ("Done!", file=stderr)
+if verbose:
+    print ("Max speed %.2fkB/s; Avg speed %.2fkB/s"% (max_speed, avg_speed), file=stderr)
--- ptpserver.py
+++ ptpserver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import socket
+import select
 import argparse
 from sys import stdin, stderr
 from math import ceil
@@ -8,8 +9,17 @@ from operator import add
 from functools import reduce
 
 
-def hit_port(server_port, client_port):
-    server_socket = socket.socket()
+def hit_port_udp(server_port, client_port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind((server_ip, server_port))
+    # print ("Hitting port ", server_port, client_port, file=stderr)
+    server_socket.sendto(b"", (client, client_port))
+    server_socket.close()
+
+
+def hit_port_tcp(server_port, client_port):
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((server_ip, server_port))
     # print ("Hitting port ", server_port, client_port, file=stderr)
@@ -21,59 +31,27 @@ def resolve_ports(bit_seq, server_is_idx, idx):
     server_port = server_offset + 1 + [0, idx][server_is_idx]
     client_port = client_offset + 1 + [idx, 0][server_is_idx]
 
-    if len(bit_seq) == bits:
-        if len(set(bit_seq)) == 1:
-            if server_is_idx:
-                return resolve_ports(bit_seq, False, idx)
-            server_port += max_index + int(bit_seq[0])
-        else:
-            client_port += int(bit_seq, 2) - 1
-    else:
+    if len(set(bit_seq)) == 1:
         if server_is_idx:
             return resolve_ports(bit_seq, False, idx)
-        global eof_offset
-        eof_offset = bits - len(bit_seq) + 1
+        server_port += max_index + int(bit_seq[0])
+    else:
+        if bit_seq[0] == "+":
+            if server_is_idx:
+                return resolve_ports(bit_seq, False, idx)
+            server_port += max_index + eval(bit_seq) + 2
+        else:
+            client_port += int(bit_seq, 2) - 1
+    hit_port_udp(server_port, client_port)
 
-        resolve_ports(bit_seq.ljust(bits, "0"), True, idx)
-        client_port += 1
-        server_port += max_index + eof_offset + 1
-        print ("Sending EOF-%d" % (eof_offset - 1))
-    hit_port(server_port, client_port)
 
-
-ap = argparse.ArgumentParser(
-    formatter_class=argparse.RawDescriptionHelpFormatter,
-    description="""
-This utility sends files from server device (this machine) to a specified client device on which an instance of ptpclient is running.
-
-Useful when you wish to communicate with another device which you can hit it's ports but cannot write data to same port and/or vice versa.
-Could also double for making sure your communication with client device is indecipherable/uninterceptable.
-
-Each socket connection (of which there are several) is represented like this:
-
-Socket Server-Side (localhost, server_port) => Socket Client-Side (client_IP, client_port)
-
-For each socket connection, the server hits a listening ${client_port} on specified ${client_IP} with it's own socket running on ${server_port} sending a bit-sequence ${bits} characters long. The bit-sequence is inferred client-side from the value of ${client_port} hit, and the position of the bit-sequence in bit stream data is inferred from the value of ${server_port}.
-
-Values of server_port are selected using the legend, with ${server_offset}+1 mapped to first item and so on:
-index 1, index 2, index 3, ..., index ${max_index}, binary(0), binary(2^${bits}-1), EOF-0, EOF-1, ..., EOF-15
-
-Values of client_port are selected using the legend, with ${client_offset}+1 mapped to first item and so on:
-binary(1), binary(2), ..., binary(2^${bits}-2)           [binary sequences are zero-filled to length ${bits}]
-
-Some server ports represent something other than indexes:
-The EOF bits are sent at the end of the broadcast to know how many bits to right strip from received data. EOF-4 means strip off last 4 bits from sequence then terminate listening on client as all data has been received.
-The null bit is also sent from server-side as it cannot be squeezed into the client port when ${bits} value is 16.
-The last bit-sequence in bit-space is also sent from server side, as port ${poll_port} is reserved for polling server (to avoid clash when ${bits} is 16)
-
-Each time the server sends the next set of ${max_index} bit-sequences, it waits by listening on port ${poll_port} for the client to state it has received, processed and properly ordered all bit-sequences received, using the accompanying indexes inferred from ${server_port}. The next set of ${max_index} bit-sequences are then sent and the process repeated till completion. This is to prevent ambiguity in where to position bits and ensure the transfer runs as quickly as (possibly varying) network speeds allow.""",
-)
-
+ap = argparse.ArgumentParser()
 ap.add_argument("-O","--server_offset",default=34000,type=int,help="Number of ports to step over before mapping offset+1, ..., to indexes. Default 34000 (in case running both server and client on same machine limit clashes)",)
 ap.add_argument("-o","--client_offset",default=1024,type=int,help="Number of ports to step over before mapping offset+1, ..., to bit sequences. Default 1024 (running non-root)",)
 ap.add_argument("-m","--max_index",default=248,type=int,help="Number of bit-sequences to send before waiting for acknowledgment from client",)
 ap.add_argument("-b","--bits",default=8,type=int,help="Bit space assigned to each port. Default 8 bits",)
 ap.add_argument("-f", "--file", default="-", type=str, help="Input file to serve. Default stdin")
+ap.add_argument("-w", "--windows_mode", action="store_true", help="Run in Windows-compatible mode")
 ap.add_argument("-i","--ip",default="0.0.0.0",type=str,help="IP address of this machine. Default 0.0.0.0",)
 ap.add_argument("-c","--client",default="127.0.0.1",type=str,help="Client IP to serve file to. Default localhost",)
 ap.add_argument("-p","--poll_port",default=65535,type=int,help="Port to hit server on to receive next set of bits. Default 65535",)
@@ -83,6 +61,7 @@ client = args["client"]
 input_stream = args["file"]
 server_ip = args["ip"]
 poll_port = args["poll_port"]
+windows_mode = args["windows_mode"]
 
 if args["bits"] < 4:
     print ("Minimum bits is 4, using ", 4, file=stderr)
@@ -107,42 +86,88 @@ if args["server_offset"] > 65535 - 19 - max_index:
 server_offset = min(args["server_offset"], 65535 - 19 - max_index)
 
 if input_stream == "-":
-    bytes = stdin
+    bytes = stdin.buffer
 else:
     bytes = open(input_stream, "rb")
 
+if windows_mode:
+    port_array = []
+else:
+    poller = select.poll()
+    fd_to_socket = {}
+    READ_ONLY = select.POLLIN | select.POLLPRI | select.POLLHUP | select.POLLERR
+
+# Set up TCP index listeners
+for port in range(server_offset + 1, server_offset + max_index + 1):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # print ("listening on", port, file=stderr)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((server_ip, port))
+    # max value in /proc/sys/net/core/somaxconn, increase if higher than 128
+    sock.listen(128)
+    if not windows_mode:
+        poller.register(sock, READ_ONLY)
+        fd_to_socket[sock.fileno()] = sock
+    else:
+        port_array.append(sock)
+
+EOF = False
+missing_count = 0
 chunksize = int(max_index / 8) * bits
-eof_offset = -1
-idx = 0
-# Set up server-poll socket
-wait_socket = socket.socket()
-wait_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-wait_socket.bind((server_ip, poll_port))
-wait_socket.listen(128)
 
 while True:
-    chunks = bytes.read(chunksize)
-    if not chunks:
-        break
-    bit_seq = reduce(add, map(lambda x: bin(x)[2:].zfill(8), chunks))
-    segments = min(max_index, int(ceil(len(bit_seq) / bits)))
-    for idx in range(segments):
+    if not missing_count:
+        chunks = bytes.read(chunksize)
+        if not chunks and EOF:
+            break
+        bit_seq = reduce(add, map(lambda x: bin(x)[2:].zfill(8), chunks), "")
+        segments = min(max_index, int(ceil(len(bit_seq) / bits)))
+        if segments != max_index:
+            EOF = True
+            EOF_offset = (bits - len(bit_seq)) % bits
+            bit_seq += "0" * EOF_offset
+            bit_seq += str(EOF_offset).rjust(bits,"+")
+            segments += 1
+    for idx in missing_indexes if missing_count else range(segments):
         to_send = bit_seq[idx * bits : (idx + 1) * bits]
+        # Send Data UDP
         resolve_ports(to_send, True, idx)
-    
-    # Wait for client ACK if not finished
-    if segments == max_index:
-        idx = -1  # Assert (idx + 2) % max_index is next index
-        while True:
-            recv_socket, (recv_ip, recv_port) = wait_socket.accept()
-            if recv_ip == client:
-                break
+    #print ("Sent data UDP")
+    # Notify client that data sent
+    hit_port_tcp(0, poll_port)
+    #print ("Notified send finish")
+    # Get count of missing indexes
+    if windows_mode:
+        readable, _, _ = select.select(port_array, [], [])
+    else:
+        readable = poller.poll()
+    ready_server = readable[0]
+    if not windows_mode:
+        ready_server = fd_to_socket[ready_server[0]]
+    server_port = ready_server.getsockname()[1]
+    recv_socket, (recv_ip, recv_port) = ready_server.accept()
+    recv_socket.close()
+    missing_count = server_port - server_offset - 1
+    if missing_count:
+        print ("Received count missing=", missing_count)
+    # Notify client that count received
+    hit_port_tcp(0, poll_port)
+    #print ("Notified count received")
+    # Receive ${missing_count} indexes missing
+    count, missing_indexes = 0, []
+    while count < missing_count:
+        if windows_mode:
+            readable, _, _ = select.select(port_array, [], [])
+        else:
+            readable = poller.poll()
+        for ready_server in readable:
+            if not windows_mode:
+                ready_server = fd_to_socket[ready_server[0]]
+            count += 1
+            server_port = ready_server.getsockname()[1]
+            recv_socket, (recv_ip, recv_port) = ready_server.accept()
+            recv_socket.close()
+            missing_indexes.append(server_port - server_offset - 1)
+    #print ("Received indexes", missing_indexes)
 
-# Handle EOF-0 when no rstrip was needed
-if eof_offset == -1:
-    print ("Sending EOF-0")
-    client_port = client_offset + (idx + 2) % max_index
-    server_port = server_offset + max_index + 3
-    hit_port(server_port, client_port)
-wait_socket.close()
 print ("Done!")
