e43fb22621215d4e75e966a74522b5bae14aed4a
lordfme <fortmax120@gmail.com>
2020-08-02 21:58:42 +0100

    rm source port tcp

--- socat_relay.sh
+++ socat_relay.sh
@@ -8,5 +8,5 @@ FWD_SOURCE_PORT=${SOCAT_PEER_PORT}
 if [ $MODE = "UDP" ];then
     socat UDP:${FWD_TO_IP}:${FWD_TO_PORT},sp=${SOCAT_PEERPORT},reuseaddr SYSTEM:"echo p"
 elif [ $MODE = "TCP" ];then
-    socat TCP:${FWD_TO_IP}:${FWD_TO_PORT},sp=${SOCAT_PEERPORT},reuseaddr SYSTEM:"echo p"
+    socat TCP:${FWD_TO_IP}:${FWD_TO_PORT}, SYSTEM:"echo p"
 fi
